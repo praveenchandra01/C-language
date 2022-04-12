584e2d156b7afa25aa0cc3831c8c1185d84fcc7b
Praveen Chandra <pravennchandra273v@gmail.com>
2022-04-13 00:01:12 +0530

    final

--- Recursion/Print_N_odd_natural_num.c
+++ Recursion/Print_N_odd_natural_num.c
@@ -1,17 +1,24 @@
 #include<stdio.h>
-int Odd_N(int i,int n)
-{
-  if(i>n)
-   return 0;
+// even no. under N;
+void even_under_N(int i, int n){
+  if(i>n) return;
   printf("%d ",i);
-  Odd_N(i+2,n);
-
+  even_under_N(i+2,n);
 }
+// total n even no.
+void even_total_N(int i, int n){
+  if(n==0) return;
+  printf("%d ",i);
+  even_total_N(i+2,n-1);
+}
+
 int main()
 {
   int n,i=1;
   printf("Enter limit : ");
   scanf("%d",&n);
-  Odd_N(i,n);
+  even_under_N(i,n);
+  printf("\n");
+  even_total_N(i,n);
   return 0;
 }
